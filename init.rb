205498c19591959e9b2f280e0b761799617398e9
Tiago Botelho <tiagonbotelho@hotmail.com>
2015-03-14 14:23:26 +0000

    can now add any url and any text file

--- init.rb
+++ init.rb
@@ -5,9 +5,16 @@ require 'birthday_bot'
 require 'rufus-scheduler'
 require 'httparty'
 
+#creates a scheduler that will check for birthdays at a specific time
+puts "Welcome to BirthdayBot"
+puts 'Please enter your slack url:' 
+url = gets.chomp
+puts 'Please enter your text file with the birth dates and names'
+namefile = gets.chomp
+
 scheduler = Rufus::Scheduler.new
-scheduler.at "14:00:00" do
-	bot = SlackBot.new("your slack url here!", 'your text file here!')
+scheduler.at "14:23:00" do
+	bot = SlackBot.new(url, namefile)
 	
 	bot.launch!
 	end

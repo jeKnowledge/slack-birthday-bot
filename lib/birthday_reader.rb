bf0981c6979df614f489b70641c41c308c9145d9
Diogo Nunes <dnunes@equalexperts.com>
2016-06-25 19:39:52 +0100

    REFACTOR: review and improve code that reads the birthdays' file
    - rename files to have names similar to their respective classes

--- lib/birthday_reader.rb
+++ lib/birthday_reader.rb
@@ -1,4 +1,4 @@
-class Birthday
+class BirthdayReader
   @@filepath = nil
 
   def self.filepath=(path=nil)
@@ -6,29 +6,25 @@ class Birthday
   end
 
   #checks if the file we are looking exists in the correct directory
-  def self.file_exists?
-    if @@filepath && File.exists?(@@filepath)
-      return true
-    else
-      return false
-    end
+  def self.file_exist?
+    return (@@filepath and File.exist?(@@filepath))
   end
 
   #checks if the file is valid
   def self.file_usable?
     return false unless @@filepath
-    return false unless File.exists?(@@filepath)
+    return false unless File.exist?(@@filepath)
     return false unless File.readable?(@@filepath)
     return true
   end
 
   #if file doesnt exist it creates one
   def self.create_file
-    File.open(@@filepath, 'w') unless file_exists?
+    File.open(@@filepath, 'w') unless file_exist?
     return file_usable?
   end
 
-  #gets the array of that line in predefined order(1st name, last name, year, month, day)
+  #gets the array of that line in predefined order (1st name, last name, year, month, day)
   def import_line(line)
     line_array = line.split(' ')
     @first_name, @last_name, @year, @month, @day = line_array
@@ -39,11 +35,15 @@ class Birthday
   def self.get_birthdays
     birthdays = []
     if file_usable?
+      print 'Parsing names and birthdays (count='
       file = File.new(@@filepath, 'r')
       file.each_line do |line|
-        birthdays << Birthday.new.import_line(line.chomp)
+        birthdays << BirthdayReader.new.import_line(line.chomp)
       end
+      puts "#{birthdays.length})"
       file.close
+    else
+      abort 'ERROR: Birthday file is not usable'
     end
     return birthdays
   end

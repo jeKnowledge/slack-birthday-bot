8274bc3a4a99485ba2fc9fd5b762d3e27437c672
shine <hello@shinenelson.com>
2017-05-28 20:53:03 +0530

    Change birthdays database to YAML / JSON
    Parsing plain text files requires more processing than
    structured file types like YAML / JSON.
    YAML support is built-into ruby > 1.9.3.
    The YAML module can easily load a JSON file too.
    
    Why was the year field removed?
    Birthdays are recurring. You need to wish people every year.
    Why would you want to torment people with the notion of them getting old? :stuck_out_tongue_winking_eye:

--- lib/birthday_reader.rb
+++ lib/birthday_reader.rb
@@ -1,12 +1,11 @@
+require 'yaml'
+
 class BirthdayReader
   def self.get_birthdays(file_path)
     birthdays = []
     if File.exist?(file_path) 
-      file = File.new(file_path, 'r')
-      file.each_line do |line|
-        birthdays << line.chomp.split
-      end
-      file.close
+	bday_list = YAML.load_file(file_path)
+	birthdays = bday_list[Time.now.month][Time.now.day]
     end
     return birthdays
   end

9b83de9852e09eb176e894f514eec1348a838e8c
shine <hello@shinenelson.com>
2017-08-01 23:21:38 +0530

    handle empty / nil YAML.load_file

--- lib/birthday_reader.rb
+++ lib/birthday_reader.rb
@@ -5,7 +5,9 @@ class BirthdayReader
     birthdays = []
     if File.exist?(file_path) 
 	bday_list = YAML.load_file(file_path)
-	birthdays = bday_list[Time.now.month.to_s][Time.now.day.to_s]
+	unless bday_list.nil? || bday_list.empty?
+          birthdays = bday_list[Time.now.month.to_s][Time.now.day.to_s]
+        end
     end
     return birthdays
   end
